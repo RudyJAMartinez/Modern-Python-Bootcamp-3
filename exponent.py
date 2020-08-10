731fdc662de1db4f248a6d13d8f02d7339e036e8
Rudy Martinez <64917902+RudyJAMartinez@users.noreply.github.com>
2020-08-10 11:25:46 -0500

    Create exponent.py

---
+++ exponent.py
@@ -0,0 +1,6 @@
+def exponent(num, power=2):
+	"""exponent(num, power)raises num to the specified power. Power defaults to 2."""
+	return num ** power
+
+print(exponent(2,3))
+print(exponent.__doc__)
