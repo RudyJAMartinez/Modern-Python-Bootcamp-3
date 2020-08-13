19f7b1e2bc9401ec3b3a00924cd4646d8e3e5ddb
Rudy Martinez <64917902+RudyJAMartinez@users.noreply.github.com>
2020-08-13 15:41:50 -0500

    Create pdb_test.py

---
+++ pdb_test.py
@@ -0,0 +1,16 @@
+import pdb
+
+first = 'First'
+second = 'Second'
+pdb.set_trace()
+result = first + second
+third = 'Third'
+result =+ third
+print(result)
+
+# pdb/set_trace()
+# l (lis)
+# n (next)
+# c (continue)
+# q (quit)
+# p (print)
