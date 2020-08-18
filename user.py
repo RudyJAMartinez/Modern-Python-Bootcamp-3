af0572ce2bad6295636d1a3402fb82cd4124d7cb
Rudy Martinez <64917902+RudyJAMartinez@users.noreply.github.com>
2020-08-17 20:34:41 -0500

    Create user.py

---
+++ user.py
@@ -0,0 +1,13 @@
+# defining a simple class that does absolutely nothing
+
+class User:
+	pass 
+user1 = User()
+user2 = User()
+user3 = User()
+user4 = User()
+
+print(type(user1))
+print(type(user2))
+print(type(user3))
+print(type(user4))
